37d8fc29fcbe59e49d27681b59384b902a0d4256
Vivek <kumarrawanivivek@gmail.com>
2024-03-27 22:38:43 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+
+     <div>
+      <h1 className='text-7xl text-emerald-700'>This is deployed at cloudflare pages</h1>
+     </div>
+
+  )
+}
+
+export default App
